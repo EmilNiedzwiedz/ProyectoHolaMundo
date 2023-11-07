c06113d6b7650e81475fb83080a9b8abab0019d8
Emil <epniedzwiedz01@educarex.es>
2023-11-07 09:20:21 +0100

    Bienvenida persona

---
+++ src/MundoHola/HolaPersona.java
@@ -0,0 +1,21 @@
+package MundoHola;
+
+import java.util.Scanner;
+
+public class HolaPersona {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String nombre;
+		
+		
+		System.out.println("Dame tu nombre");
+		
+		nombre=sc.next();
+		
+		System.out.println("Bienvenido" + nombre);
+		// TODO Auto-generated method stub
+
+	}
+
+}
